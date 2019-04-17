719a06e34f705b2f8fb4ccb8a920bdb7fd7015ce
visualstudio <visualstudio@122-PC36>
2019-04-17 10:19:16 -0400

    longestWord's

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    class Program
-    {
-        static void Main(string[] args)
+    
+
+class Program
         {
+            static void Main(string[] args)
+            {
+                Program a = new Program();
+                Console.WriteLine("The Longest word is :");
+                Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first alogirithm designed for processing by an Analytical Engine."));
+            }
+            public string MyFunctionA(string input)
+            {
+                string[] words = input.Split(' ');
+                int wordArrayLength = words.Length;
+                int[] wordLength = new int[wordArrayLength];
+                int x = 0;
+                string aa = " ";
+                foreach (var word in words)
+                {
+                    if (word.Length > x)
+                    {
+                        input = word;
+                        aa = input;
+                        x = word.Length;
+                    }
+                }
+                string currentWord = words[0];
+                string nextWord;
+                string longestWord = currentWord;
+                for (int y = 0; y < words.Length - 1; y++)
+                {
+                    currentWord = words[y];
+                    nextWord = words[y + 1];
+                }
+                return aa;
+            }
         }
     }
-}
+
